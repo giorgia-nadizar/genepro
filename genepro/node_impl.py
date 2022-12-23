10f317602618686bd54b590efbed8fc7653b1a21
Luigi Rovito <luigirovito2@gmail.com>
2022-12-23 09:19:45 +0100

    [develop] Fixed Mod protected division in node_impl

--- genepro/node_impl.py
+++ genepro/node_impl.py
@@ -74,8 +74,10 @@ class Mod(Node):
     def get_output(self, X):
         c_outs = self._get_child_outputs(X)
         # implements a protection to avoid dividing by 0
-        c_outs[1] = np.where(c_outs[1] == 0, c_outs[0], c_outs[1])
-        return np.core.umath.clip(c_outs[0], -1e+100, 1e+100) % np.core.umath.clip(c_outs[1], -1e+100, 1e+100)
+        sign_b = np.sign(c_outs[1])
+        sign_b = np.where(sign_b == 0, 1, sign_b)
+        protected_div = sign_b * np.core.umath.clip(c_outs[0], -1e+100, 1e+100) % ( np.core.umath.clip(np.abs(np.core.umath.clip(c_outs[1], -1e+100, 1e+100)), 1e-9, 1e+100))
+        return protected_div
 
 
 class Square(Node):
